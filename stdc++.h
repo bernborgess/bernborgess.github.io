f429317732a463ac6243ec61362aee8036c9caa4
bernborgess <bernborgess@outlook.com>
2026-05-27 08:09:42 -0300

    Add maratona snippets

---
+++ stdc++.h
@@ -0,0 +1,32 @@
+// Save at $HOME/.config/include/bits/stdc++.h
+#include <algorithm>  // Algorithms on sequences (e.g., sort, find)
+#include <cassert>    // Assert function for debugging
+#include <cctype>     // Character classification functions
+#include <cmath>      // Mathematical functions
+#include <cstdlib>    // General purpose functions (e.g., memory management)
+#include <cstring>    // C-style string functions
+#include <ctime>      // Date and time functions
+#include <exception>  // Standard exceptions
+#include <fstream>    // File stream objects
+#include <functional> // Function objects
+#include <iomanip>    // Input/output manipulators
+#include <iostream>   // Input/output stream objects
+#include <iterator>   // Iterator classes
+#include <limits>     // Numeric limits
+#include <list>       // Doubly linked list
+#include <locale>     // Localization and internationalization
+#include <map>        // Map container
+#include <numeric>    // Numeric operations (e.g., accumulate)
+#include <queue>      // Queue container
+#include <random>     // Random number generators
+#include <set>        // Set container
+#include <sstream>    // String stream objects
+#include <stack>      // Stack container
+#include <stdexcept>  // Standard exception classes
+#include <string>     // String class and functions
+#include <typeinfo>   // Runtime type information
+#include <utility>    // Utility components (e.g., std::pair)
+#include <vector>     // Dynamic array
+
+#include <unordered_map>
+#include <unordered_set>
